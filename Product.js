5034d4e7d971b00ce683981d2930662b276e9795
Arneliyo <72642460+Arneliyo@users.noreply.github.com>
2020-11-05 15:24:49 +0800

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,1 +1,30 @@
 
+angular.module('shopApp', [])
+ .controller('productController', function ($scope) {
+ var id = +utils.getQueryParams()['id'],
+ p = repository.getProduct(id);
+ $scope.name = p.name;
+ $scope.price = p.price;
+ $scope.description = p.description;
+ $scope.category = p.category;
+ });
+var utils = (function () {
+ return {
+ getQueryParams: function () {
+ var qs = document.location.search.split('+').join(' '),
+ params = {},
+ tokens,
+ regex = /[?&]?([^=]+)=([^&]*)/g;
+ while (tokens = regex.exec(qs)) {
+ params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+ }
+ return params;
+ }
+ };
+})();
+Search.js
+angular.module('shopApp', [])
+ .controller('searchController', function ($scope) {
+ var q = utils.getQueryParams()['q'];
+ $scope.results = repository.search(q);
+ });
